008cd37ec7570538245eb0d4c100ceb06624ead1
WagnerReis <wagnerreis1997@gmail.com>
2022-10-19 17:23:08 -0300

    API Gateway [Event Emitter]

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Logger,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private logger = new Logger(AppController.name);
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  private clientAdminBackend: ClientProxy;
+
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://user:bitnami@localhost:5672/smartranking'],
+        queue: 'admin-backned',
+      },
+    });
+  }
+
+  @Post('categorias')
+  @UsePipes(ValidationPipe)
+  async criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDto) {
+    return await this.clientAdminBackend.emit(
+      'criar-categoria',
+      criarCategoriaDto,
+    );
   }
 }
